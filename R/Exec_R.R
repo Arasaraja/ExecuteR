f12859011937cc6dcc594b6ac13ffbcac1060395
Arasaraja <arasa_raja@yahoo.com>
2017-07-07 17:25:16 +0530

    Add files via upload

---
+++ R/Exec_R.R
@@ -0,0 +1,28 @@
+# Linear Regression
+#
+# This function takes one data parameter and applies linear Regression which returns plot.
+
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+# a=1:10;
+# b=13:22;
+# c=6:15;
+# data=data.frame(a,b,c)
+
+data  <- read.csv("Data/data.csv")
+
+Exec_R=function(data) {
+  lin_reg=lm(a~., data=data)
+  return(plot(lin_reg))
+
+}
+
+
